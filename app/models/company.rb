e7e6f8e88d78800509d5c4cee653714ff72dac0d
Dave Desrochers <dave726@gmail.com>
2012-09-29 16:15:01 -0700

    Added company model

---
+++ app/models/company.rb
@@ -0,0 +1,3 @@
+class Company < ActiveRecord::Base
+  attr_accessible :name, :permalink, :category_code
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120929185502) do
+ActiveRecord::Schema.define(:version => 20120929204255) do
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.string   "permalink"
+    t.string   "category_code"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
   create_table "recons", :force => true do |t|
     t.string   "company_name"
